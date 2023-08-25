b359e197109a67558efcf7687139d4c1b4b97d8d
admin <rohitshivhare8381@gmail.com>
2023-08-25 13:24:06 +0530

    oops commt

--- OOPS_1.py
+++ OOPS_1.py
@@ -55,23 +55,137 @@
 
 # =========================================
 
-b = 10
+# b = 10
+#
+# class test:
+#
+#     c =20
+#     def __init__(self):
+#         self.a = 10
+#
+#     def emp_1(self):
+#         self.e = 30
+#         print(self.a)
+#
+#
+# em = test()
+# print(em.a)
+# print(b)
+# print(em.c)
 
-class test:
+# -==========================================================
+# decorators - extending the functinality of class and methods.
+# b = 10
+#
+# class test:
+#
+#     c = 20
+#     def __init__(self):
+#         self.a = 10
+# em = test()
+#
+# t1 = test()
+# t2 = test()
+#
+# print("t1 : " , t1.c , t1.a)
+# print("t2 : " , t2.c , t2.a)
+#
+# print("t1 changed " , t1.c  , t1.a)
+# print("t2 changed " , t2.c  , t2.a)
+#
+# test.c = 800
+# t1.a = 400
+#
+# print("t1 changed " , t1.c  , t1.a)
+# print("t2 changed " , t2.c  , t2.a)
 
-    c =20
-    def __init__(self):
-        self.a = 10
 
-    def emp_1(self):
-        self.e = 30
-        print(self.a)
+# =====================================================
+
+# class animal:
+#     legs = 4
+#     @classmethod
+#     def walk(self , name):
+#         print('{} walks with {} legs'.format(name , self.legs))
+#
+# animal.walk("cat")
+#
+
+# ==========================================
+
+#
+# classmethod
+# staticmethod
+
+# class test():
+#     a=122
+#     def __init__(self):
+#         test.a = 20
+#
+#     @classmethod
+#     def m1(cls):
+#         cls.a = "one piece"
+#         test.c = "naruto"
+#     # @classmethod
+#     def m2(self):
+#         test.a = "rohit"
+#
+#     def m3(self):
+#         test.a = "it"
+#         self.y = "cs"
+#
+# t= test()
+# print(t.a)
+#
+# t.m1()
+# print(t.a)
+#
+# t.m2()
+# print(t.a)
 
 
-em = test()
-print(em.a)
-print(b)
-print(em.c)
+# ============================================== #
+# class ShivhareRohit:
+#     @staticmethod
+#     def add(x,y):
+#         print(x+y)
+#     @staticmethod
+#     def sub(x,y):
+#         print(x-y)
+#     def mul(x,y):
+#         print(x*y)
+#     def div(x,y):
+#         print(x/y)
+#
+# # l= ShivhareRohit()
+# ShivhareRohit.add(10,29)
+# ShivhareRohit.sub(10,29)
+# ShivhareRohit.mul(10,29)
+# ShivhareRohit.div(10,29)
+
+# ===================================================
+
+# class employee:
+#     def __init__(self,id,name,sal):
+#         self.name = name
+#         self.id = id
+#         self.sal = sal
+#     def display(self):
+#         print("emp id :" , self.id)
+#         print("emp name :" , self.name)
+#         print("emp salary :",self.sal)
+# class test:
+#     def modify(pc):
+#         pc.sal = pc.sal+1000
+# d = employee(10,"Rohit",300)
+# d.display()
+# e = employee(20,"pradeep",1)
+# test.modify(e)
+# e.display()
+
+
+# ============================================================
+
 
 
 
--- Password_generator.py
+++ Password_generator.py
@@ -1,7 +1,17 @@
-# PASSWORD GNERATOR
+# # PASSWORD GNERATOR
+import random
+# import re
 
+length_from_user = int(input("Enter length of your Password  ==> "))
+int_1 = ["1","2","3","4","5","6","7","9","0" , "@","#","$","%","^","&","*",
+         "a","q","w","e","r","t","y","u","i","o","","","","","","","","","","","","","","","",""]
 
-
-
-
+if length_from_user > 4 and length_from_user < 13:
+    t = ""
+    for i in range(0,length_from_user):
+        r = random.choice(int_1)
+        t = t+r
+    print(t)
+else:
+    print("enter approciate lengeth for your password from 5 to 12")
 
